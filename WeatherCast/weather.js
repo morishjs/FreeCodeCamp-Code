c7b072beacc4d62b9f585f0f11e9f1e96d269907
Junsuk Park <park64kr63@gmail.com>
2016-07-18 00:06:29 +0900

    Add celcius
    Need to fix the align with the context.

--- WeatherCast/weather.js
+++ WeatherCast/weather.js
@@ -13,14 +13,17 @@ function getWeather() {
         //lat, lon are valid in function scope.
         lat = data.lat;
         lon = data.lon;
-        alert(lat);
+
         quaryParams = "?lat=" + lat;
         quaryParams += "&lon=" + lon;
         quaryParams += "&units=" + "metric";
         quaryParams += "&APPID=" + "c4a1f946faca07dbee1c39448d1e2d47";
         $.getJSON(url + quaryParams, function(data) {
             var temp = data.main.temp;
-
+            temp = Math.ceil(temp);
+            $('#celcius').html(
+              "<div id='celcius'>" +
+              temp+' '+"&#8451;" + "</div>");
         });
     });
 
