52dfb0a48b980385b7ea0c267868b9f40b6f363b
Junsuk Park <park64kr63@gmail.com>
2016-07-17 22:58:51 +0900

    Weathercast
    display weather case

---
+++ WeatherCast/weather.js
@@ -0,0 +1,33 @@
+function getWeather() {
+    //get weather information from the url
+    var quaryParams;
+    var url;
+    var lat;
+
+    var lon;
+
+
+    url = "http://api.openweathermap.org/data/2.5/weather";
+
+    $.getJSON("http://ip-api.com/json", function(data) {
+        //lat, lon are valid in function scope.
+        lat = data.lat;
+        lon = data.lon;
+        alert(lat);
+        quaryParams = "?lat=" + lat;
+        quaryParams += "&lon=" + lon;
+        quaryParams += "&units=" + "metric";
+        quaryParams += "&APPID=" + "c4a1f946faca07dbee1c39448d1e2d47";
+        $.getJSON(url + quaryParams, function(data) {
+            var temp = data.main.temp;
+
+        });
+    });
+
+
+
+}
+
+$(document).ready(function() {
+    getWeather();
+});
